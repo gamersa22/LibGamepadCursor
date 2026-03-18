55726c7bb5a2ec7f03e6324861c62b5693d26b29
gamersa22 <140577703+gamersa22@users.noreply.github.com>
2026-03-18 15:17:35 +1030

    added get handlers
    Added support for a enter, exit, clicked, down and up mouse handlers.
    Bug: trying to interact fails with an error, or your character will jump.
    fixed the joystick buy setting to ui mode every frame, need better fix.

--- LibGamepadCursor.lua
+++ LibGamepadCursor.lua
@@ -5,7 +5,13 @@ GAMEPAD_CURSOR_TARGET_TYPES =
     INTERACTABLE = 1, -- Buttons, Sliders, etc.
     TEXT = 2,         -- Labels
 }
-
+local TRACKED_MOUSE_HANDLERS = {
+	ON_MOUSE_ENTER = "OnMouseEnter",
+	ON_MOUSE_EXIT = "OnMouseExit",
+	ON_MOUSE_DOWN = "OnMouseDown",
+	ON_MOUSE_UP = "OnMouseUp",
+	ON_CLICKED = "OnClicked",
+}
 -- Friction factors: how much the cursor slows down when hovering
 GAMEPAD_CURSOR_FRICTION_FACTORS =
 {
@@ -25,22 +31,24 @@ LibGamepadCursor = ZO_InitializingCallbackObject:Subclass()
 
 function LibGamepadCursor:Initialize(control)
     self.control = control
+	self.savedHandlers = {}
+	self:CreateKeybinds()
     self:Reset()
 end
 
 function LibGamepadCursor:Reset()
     self.x, self.y = GuiRoot:GetCenter()
     self.frictionInterpolationFactor = 1
-
+	
     if self.objectTypeUnderCursor then
         self:ResetObjectUnderCursor()
     else
         self.objectTypeUnderCursor = GAMEPAD_CURSOR_TARGET_TYPES.NONE
         self.objectUnderCursor = nil
     end
-
     self.control:SetAnchor(CENTER, GuiRoot, TOPLEFT, self.x, self.y)
     self.control:SetHidden(true)
+	
     self:RefreshObjectUnderCursor()
 end
 
@@ -48,7 +56,7 @@ function LibGamepadCursor:UpdateDirectionalInput()
     local cursorMoved = false
     -- Get input from both Left Stick and D-Pad
     local dx, dy = DIRECTIONAL_INPUT:GetXY(ZO_DI_LEFT_STICK)
-    
+    SetGamepadLeftStickConsumedByUI(true)
     if dx ~= 0 or dy ~= 0 then
         dx, dy = zo_clampLength2D(dx, dy, 1.0)
         local frameDelta = GetFrameDeltaNormalizedForTargetFramerate()
@@ -71,50 +79,55 @@ function LibGamepadCursor:UpdateDirectionalInput()
     if cursorMoved then
         self:OnCursorPositionChanged()
     end
+	 SCENE_MANAGER:SetInUIMode(true)
 end
 
 function LibGamepadCursor:OnCursorPositionChanged()
     self:FireCallbacks("CursorPositionChanged", self.x, self.y)
 end
-local myKeybindDescriptor = {
-    alignment = KEYBIND_STRIP_ALIGN_CENTER,
-    {
-        name = "Select",
-        keybind = "UI_SHORTCUT_PRIMARY", -- This is the 'A' button on Xbox / 'Cross' on PS
-        callback = function()
-            -- This is where we check what the cursor is touching
-			--###############################
-			-- gamepadCursor might need to be swapped to self
-			--###############################
-            local target, targetType = gamepadCursor:GetObjectUnderCursor()
+function LibGamepadCursor:CreateKeybinds()
+	self.keybinds = {
+		alignment = KEYBIND_STRIP_ALIGN_CENTER,
+		{
+			name = "Select",
+			keybind = "UI_SHORTCUT_PRIMARY", -- This is the 'A' button on Xbox / 'Cross' on PS
+			callback = function()
+				-- This is where we check what the cursor is touching
+				--###############################
+				-- gamepadCursor might need to be swapped to self
+				--###############################
+				--local target, targetType = self:GetObjectUnderCursor()
 
-            if targetType == GAMEPAD_CURSOR_TARGET_TYPES.INTERACTABLE and target then
-                -- Safely trigger the button's behavior
-                local handler = target:GetHandler("OnMouseUp")
-                if handler then
-                    handler(target, 0, false)
-                    PlaySound(SOUNDS.DIALOG_ACCEPT)
-                end
-            end
-        end,
-    },
-}
+				--if targetType == GAMEPAD_CURSOR_TARGET_TYPES.INTERACTABLE and target then
+					-- Safely trigger the button's behavior
+					if self.savedHandlers[TRACKED_MOUSE_HANDLERS.ON_CLICKED] then self.savedHandlers[TRACKED_MOUSE_HANDLERS.ON_CLICKED](self.objectUnderCursor,1) end
+					if self.savedHandlers[TRACKED_MOUSE_HANDLERS.ON_MOUSE_DOWN] then self.savedHandlers[TRACKED_MOUSE_HANDLERS.ON_MOUSE_DOWN](self.objectUnderCursor,1) end
+					if self.savedHandlers[TRACKED_MOUSE_HANDLERS.ON_MOUSE_UP] then self.savedHandlers[TRACKED_MOUSE_HANDLERS.ON_MOUSE_UP](self.objectUnderCursor,1) end
+				--end
+			end,
+		},
+	}
+end
 function LibGamepadCursor:SetActive(active)
     self.control:SetHidden(not active)
     if active then
+		SCENE_MANAGER:SetInUIMode(true)
+
+		
         -- Activate the input listener
         DIRECTIONAL_INPUT:Activate(self, self.control)
         -- IMPORTANT: Set UI Mode to true to block character and camera movement
-        SCENE_MANAGER:SetInUIMode(true)
-        KEYBIND_STRIP:AddKeybindButtonGroup(myKeybindDescriptor)
+        KEYBIND_STRIP:AddKeybindButtonGroup( self.keybinds)
+		 KEYBIND_STRIP:UpdateKeybindButtonGroup( self.keybinds)
+     
         if not self.cursorId then
             self.cursorId = WINDOW_MANAGER:CreateCursor(self.x, self.y)
         end
     else
+		KEYBIND_STRIP:RemoveKeybindButtonGroup( self.keybinds)
         -- Deactivate input listener and return to world mode
         DIRECTIONAL_INPUT:Deactivate(self)
         SCENE_MANAGER:SetInUIMode(false)
-        KEYBIND_STRIP:RemoveKeybindButtonGroup(myKeybindDescriptor)
         if self.cursorId then
             WINDOW_MANAGER:DestroyCursor(self.cursorId)
             self.cursorId = nil
@@ -138,7 +151,7 @@ function LibGamepadCursor:RefreshObjectUnderCursor()
     WINDOW_MANAGER:UpdateCursorPosition(self.cursorId, self.x, self.y)
     local targetControl = WINDOW_MANAGER:GetControlAtCursor(self.cursorId)
     local objectType = GAMEPAD_CURSOR_TARGET_TYPES.NONE
-    local isUnderCursor = false
+    local isUnderCursor = true
 
     if targetControl then
         local controlType = targetControl:GetType()
@@ -159,14 +172,24 @@ end
 
 function LibGamepadCursor:SetObjectUnderCursor(object, objectType, isUnderCursor)
     local previousObjectType = self.objectTypeUnderCursor
-    
     if isUnderCursor or objectType ~= previousObjectType or objectType ~= GAMEPAD_CURSOR_TARGET_TYPES.NONE then
         local previousObject = self.objectUnderCursor
         if (not isUnderCursor) or object ~= previousObject then
+			if self.savedHandlers[TRACKED_MOUSE_HANDLERS.ON_MOUSE_EXIT] then self.savedHandlers[TRACKED_MOUSE_HANDLERS.ON_MOUSE_EXIT](previousObject) end
+			self.savedHandlers = {}
             if isUnderCursor then
                 self.objectUnderCursor = object
                 self.objectTypeUnderCursor = objectType
-				self.objectUnderCursor:GetHandler("OnMouseEnter")(self.control)
+				d("ObjectName: "..self.objectUnderCursor:GetName())
+				for _, handlerName in pairs(TRACKED_MOUSE_HANDLERS) do
+					local handler = self.objectUnderCursor:GetHandler(handlerName)
+					if handler then 	
+						self.savedHandlers[handlerName] = handler 
+						d(handlerName.." Found")
+					end
+					
+				end
+				if self.savedHandlers[TRACKED_MOUSE_HANDLERS.ON_MOUSE_ENTER] then self.savedHandlers[TRACKED_MOUSE_HANDLERS.ON_MOUSE_ENTER](self.objectUnderCursor, self.control) end
             else
                 self.objectUnderCursor = nil
                 self.objectTypeUnderCursor = GAMEPAD_CURSOR_TARGET_TYPES.NONE
@@ -196,6 +219,7 @@ local function OnAddonLoaded(event, name)
 		return
 	end
 	EVENT_MANAGER:UnregisterForEvent(addonname, EVENT_ADD_ON_LOADED)
+	
    	gamepadCursor = LibGamepadCursor:New(LibGamepadCursor_TopLevelGamepadCursor)
 
 	SLASH_COMMANDS["/lgc"]=function()
